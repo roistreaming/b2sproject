4514bc0f925d97c63384a637dd834d05271f38d7
roistreaming <120103899+roistreaming@users.noreply.github.com>
2022-12-09 15:40:40 +0000

    Create model.py

---
+++ model/model.py
@@ -0,0 +1,22 @@
+import json
+
+example_string = '{"datetime": "2022-12-09 09:17:32.525727", "orderid": 1, "storeid": 35, "registerid": 6, "cashierid": 1, "customerid": 464, "sku": 6137, "qty": 2, "price": 80.8, "stock_qty": 0}'
+
+def is_suspicious(s):
+    example_json = json.loads(example_string)
+    purchase_qty = example_json['qty']
+    stock_qty = example_json['stock_qty']
+    suspicious = 0
+    
+    if purchase_qty > stock_qty:
+        suspicious = 1
+    else:
+        suspicious = 0
+
+    # print(example_json['qty'])
+    # print(example_json['stock_qty'])
+    # print(suspicious)
+
+    return suspicious
+
+is_suspicious(example_string)
